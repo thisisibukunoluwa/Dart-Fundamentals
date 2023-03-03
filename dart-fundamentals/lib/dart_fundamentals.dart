174723877d2df145b17f6195911ceb2c555f69cc
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-03 23:55:18 +0100

    feat:added Math methods

---
+++ dart-fundamentals/lib/dart_fundamentals.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+void main() {
+  print('Hello World');
+  //Math 
+  print(45 ~/ 7);
+  print(cos(135 * pi / 180));
+  print(max(5,10),);
+  print(min(39, 18),);
+  //Mini Exercise 1 
+  //Print the value of 1 over the square root of 2 in dart. Confirm that it equals the sine of 45
+  
+
+  // Naming data 
+}
