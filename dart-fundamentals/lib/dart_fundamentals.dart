36d00bdd9b4f218b86fdb392feec304dd2455f57
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-04 01:32:53 +0100

    feat:added types

--- dart-fundamentals/lib/dart_fundamentals.dart
+++ dart-fundamentals/lib/dart_fundamentals.dart
@@ -2,14 +2,29 @@ import 'dart:math';
 
 void main() {
   print('Hello World');
-  //Math 
+  //Math
   print(45 ~/ 7);
   print(cos(135 * pi / 180));
-  print(max(5,10),);
-  print(min(39, 18),);
-  //Mini Exercise 1 
+  print(
+    max(5, 10),
+  );
+  print(
+    min(39, 18),
+  );
+  //Mini Exercise 1
   //Print the value of 1 over the square root of 2 in dart. Confirm that it equals the sine of 45
-  
+  print(1 / sqrt(2));
 
-  // Naming data 
+  // Naming data
+  int number = 10;
+  number = 15;
+
+  double apple = 3.14159;
+  print(10.isEven);
+  print(3.1345.round());
+
+  dynamic myVariable;
+  myVariable = 10;
+  myVariable = 'hello';
+  myVariable = 134.232;
 }
